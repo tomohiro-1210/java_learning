20232fb2652067f71345ca53589c6ba24e8ee9ae
tomohiro_0225 <express22212270@gmail.com>
2024-05-29 17:11:25 +0900

    paizaの学習04途中兼pushテスト

---
+++ paiza_beginner01/paiza_beginner01-04.java
@@ -0,0 +1,11 @@
+// HTMLを表示する
+public class Main {
+	public static void main(String[] args) {
+	    System.out.println("<h1>ミミックがあらわれた！</h1>");
+	    System.out.println("<p>勇者のこうげき！</p>");
+	    System.out.println("<style> h1{ color: #fb0077; } </style>");
+	    System.out.println("<style> p{ color:red; } </style>");
+	}
+}
+
+// 演習課題1
